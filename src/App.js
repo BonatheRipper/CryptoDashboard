f9289a9f6f458c0f82f7c953cfa347954a1dc8d7
bonatheripper <izeoguandrew@gmail.com>
2022-10-29 16:04:56 +0100

    added many files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-green-500 text-4xl">
+      AppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppAppApp App App
+      App App AppApp App App App App AppApp App App App App AppApp AppApp App
+      App App App App
+    </div>
+  );
+};
+
+export default App;
